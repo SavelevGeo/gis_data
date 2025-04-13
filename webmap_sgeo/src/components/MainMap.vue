dc2049644fcbba78e76b2b5c0cf9e2ce6c63446e
Georgiy Savelyev <savelevgeo.post@gmail.com>
2025-04-13 21:55:33 +0300

    add map

---
+++ webmap_sgeo/src/components/MainMap.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ol-map style="min-width: 100vw; height: 100vh">
+    <ol-view :center="[40, 40]" :zoom="5" />
+    <ol-tile-layer :opacity="0.5">
+      <ol-source-osm />
+    </ol-tile-layer>
+  </ol-map>
+</template>
--- webmap_sgeo/src/main.ts
+++ webmap_sgeo/src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import "vue3-openlayers/dist/vue3-openlayers.css";
+
+import OpenLayersMap from "vue3-openlayers";
+
+const app = createApp(App);
+
+app.use(OpenLayersMap);
+
+app.mount('#app')
